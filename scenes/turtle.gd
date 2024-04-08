32b124ee926cdbdc38de779a7b572e708f32363e
Gowthamaswa <gowtham692000@gmail.com>
2024-04-08 18:33:23 +0530

    Updated Mario-Bros

--- scenes/turtle.gd
+++ scenes/turtle.gd
@@ -1,12 +1,15 @@
 extends Enemy
 
+class_name Turtle
+
 var in_a_shell = false
 
 const TURTLE_SHELL_COLLISION_SHAPE_POSITION = Vector2(0,5)
 const TURTLE_FULL_COLLISION_SHAPE = preload("res://Resources/CollisionShapes/turtle_full_collision_shape.tres")
 const TURTLE_SHELL_COLLISION_SHAPE = preload("res://Resources/CollisionShapes/turtle_shell_collision_shape.tres")
-@onready var collision_shape_2d = $CollisionShape2D
 
+@onready var collision_shape_2d = $CollisionShape2D
+@export var sliding_speed = 200
 
 func _ready():
 	collision_shape_2d.shape = TURTLE_FULL_COLLISION_SHAPE
@@ -18,3 +21,11 @@ func die():
 	collision_shape_2d.set_deferred("shape", TURTLE_FULL_COLLISION_SHAPE)
 	collision_shape_2d.set_deferred("position", TURTLE_SHELL_COLLISION_SHAPE_POSITION)
 	in_a_shell = true
+	
+func on_stomp(player_position: Vector2):
+	set_collision_mask_value(1, false)
+	set_collision_layer_value(3, false)
+	set_collision_layer_value(4, true)
+	
+	var movement_direction = 1 if player_position.x <= global_position.x else -1
+	horizontal_speed = -movement_direction * sliding_speed
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 class_name Player
 
+signal points_scored(points: int)
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 enum PlayerMode{
@@ -20,17 +22,18 @@ enum PlayerMode{
 
 @export_group("Locomotion")
 @export var run_speed_damping = 0.5
-@export var speed =  100
+@export var speed =  200
 @export var jump_velocity = -350
 @export_group("")
 
 @export_group("stomping enemies")
 @export var min_stomp_degree = 35
-@export var mmax_stomp_degree = 140
+@export var max_stomp_degree = 140
 @export var stomp_y_velocity = -150
 @export_group("")
 
- 	
+const POINTS_LABEL_SCENE = preload("res://scenes/points_label.tscn")
+
 var player_mode = PlayerMode.SMALL
 
 func _physics_process(delta):
@@ -56,25 +59,35 @@ func _physics_process(delta):
 	animated_sprite_2d.trigger_animation(velocity, direction, player_mode)
 		
 	move_and_slide()
-	
-	
 
-func _on_area_2d_area_entered(area):
-	if area is Enemy:
-		handle_enemy_collision(area)
+#func _on_area_2d_area_entered(area):
+	#if area is Enemy:
+		#handle_enemy_collision(area)
 
-func handle_enemy_collision(enemy: Enemy):
-	if Enemy == null:
-		return
+#func handle_enemy_collision(enemy: Enemy):
+	#if Enemy == null:
+		#return
+		#
+	#if is_instance_of(enemy, Turtle) and (enemy as Turtle).in_a_shell:
+		#(enemy as Turtle). on_stomp(global_position)
+		#spawn_points_label(enemy)
+	#
+	#else:	
+		#var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
+		#
+		#if angle_of_collision > min_stomp_degree && max_stomp_degree > angle_of_collision:
+			#enemy.die()
+			#on_enemy_stomped()
+			#spawn_points_label(enemy)
+			#
+		#else:
+			#die()
 		
-	var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
-	
-	if angle_of_collision > min_stomp_degree && mmax_stomp_degree >angle_of_collision:
-		enemy.die()
-		on_enemy_stomped()
-		
-	else:
-		die()
+func spawn_points_label(enemy):
+	var points_label = POINTS_LABEL_SCENE.instantiate()
+	points_label.position = enemy.position + Vector2(-20, -20)
+	get_tree().root.add_child(points_label)
+	points_scored.emit(100)
 		
 func on_enemy_stomped():
 	velocity.y = stomp_y_velocity
